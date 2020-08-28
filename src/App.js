6a80476179b916cf1626cf2981dfb92d715ed31d
ShubhankarDas <shubh065@gmail.com>
2020-08-28 11:02:56 +0530

    Started with login

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Chat from "./Chat";
+import Message from "./Message";
 
 function App() {
+  const [user, setUser] = useState(null);
+
   return (
     <div className="app">
       <Router>
-        <Header />
-        <div className="app__body">
-          {/* Sidebar */}
-          <Sidebar />
-          <Switch>
-            <Route path="/room/:roomId">
-              <Chat />
-            </Route>
-            <Route path="/">
-              <h2>Welcome</h2>
-            </Route>
-          </Switch>
-        </div>
+        {!user ? (
+          <h1>Login page</h1>
+        ) : (
+          <>
+            <Header />
+            <div className="app__body">
+              {/* Sidebar */}
+              <Sidebar />
+              <Switch>
+                <Route path="/room/:roomId">
+                  <Chat />
+                </Route>
+                <Route path="/">
+                  <h2>Welcome</h2>
+                </Route>
+              </Switch>
+            </div>
+          </>
+        )}
       </Router>
     </div>
   );
---
+++ src/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Login.css";
+
+function Login() {
+  return (
+    <div className="login">
+      <div className="login__conatiner"></div>
+    </div>
+  );
+}
+
+export default Login;
